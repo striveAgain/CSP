eb1845fccc6e342bd41c65f5f2e5175b9ab2b425
striveAgain <105333061+striveAgain@users.noreply.github.com>
2022-11-20 21:18:07 +0800

    CSP-201512-T2

--- 201512-T2.cpp
+++ 201512-T2.cpp
@@ -62,3 +62,65 @@ int main() {
 
     return 0;
 }
+
+
+/*
+#include <iostream>
+#include <cmath>
+ 
+using namespace std;
+ 
+const int N = 30;
+ 
+int grid[N][N], n, m;
+ 
+int main()
+{
+    // 输入数据
+    cin >> n >> m;
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m; j++) {
+            cin >> grid[i][j];
+        }
+ 
+    // 进行行标记（可以消除则置为负）
+    for(int i=0; i<n; i++)
+        for(int j=0; j<m-2; j++) {
+            if(abs(grid[i][j]) == abs(grid[i][j+1]) && abs(grid[i][j+1]) == abs(grid[i][j+2])) {
+                if(grid[i][j] > 0)
+                    grid[i][j] = - grid[i][j];
+                if(grid[i][j+1] > 0)
+                    grid[i][j+1] = - grid[i][j+1];
+                if(grid[i][j+2] > 0)
+                    grid[i][j+2] = - grid[i][j+2];
+            }
+        }
+ 
+    // 进行列标记（可以消除则置为负）
+    for(int j=0; j<m; j++)
+        for(int i=0; i<n-2; i++) {
+            if(abs(grid[i][j]) == abs(grid[i+1][j]) && abs(grid[i+1][j]) == abs(grid[i+2][j])) {
+                if(grid[i][j] > 0)
+                    grid[i][j] = - grid[i][j];
+                if(grid[i+1][j] > 0)
+                    grid[i+1][j] = - grid[i+1][j];
+                if(grid[i+2][j] > 0)
+                    grid[i+2][j] = - grid[i+2][j];
+            }
+        }
+ 
+    // 进行清除（可以消除则置为0）并且输出结果
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            if(grid[i][j] < 0)
+                grid[i][j] = 0;
+            if(j != 0)
+                cout << " ";
+            cout << grid[i][j];
+        }
+        cout << endl;
+    }
+ 
+    return 0;
+}
+*/
