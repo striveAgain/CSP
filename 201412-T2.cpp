aefc404e75094bbc894801df71ef45f5818b8174
striveAgain <105333061+striveAgain@users.noreply.github.com>
2022-11-21 22:10:43 +0800

    CSP-201412-T2

--- 201412-T2.cpp
+++ 201412-T2.cpp
@@ -39,3 +39,44 @@ int main() {
 
     return 0;
 }
+
+
+/*
+solution 2
+#include <bits/stdc++.h>
+using namespace std;
+
+const int maxn = 510;
+int mat[maxn][maxn];
+int n;
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= n; ++j) {
+            cin >> mat[i][j];
+        }
+    }
+
+    for (int i = 1; i <= 2 * n; ++i) {
+        if (!(i & 1)) {
+            for (int j = 1; j <= i; ++j) {
+                int row = j, col = i - j + 1;
+                if (row >= 1 and row <= n and col >= 1 and col <= n) {
+                    cout << mat[row][col] << " ";
+                }
+            }
+        } else {
+            for (int j = 1; j <= i; ++j) {
+                int row = i - j + 1, col = j;
+                if (row >= 1 and row <= n and col >= 1 and col <= n) {
+                    cout << mat[row][col] << " ";
+                }
+            }
+        }
+    }
+
+    return 0;
+}
+
+*/
